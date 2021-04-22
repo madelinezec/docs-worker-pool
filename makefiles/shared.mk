b2bdca05524ad0bda5ffe7b97cbc9bfb50c132ac
madelinezec <mzechar@gmail.com>
2021-04-22 15:12:07 -0400

    first commit

--- makefiles/shared.mk
+++ makefiles/shared.mk
@@ -1,3 +1,4 @@
+COMMIT_HASH=$(shell git rev-parse --short HEAD)
 INTEGRATION_SEARCH_BUCKET=docs-search-indexes-integration
 
 ifeq ($(SNOOTY_INTEGRATION),true)
@@ -12,3 +13,13 @@ next-gen-deploy-search-index:
 	@echo "Building search index"
 	mut-index upload public -o ${MANIFEST_PREFIX}.json -u ${PRODUCTION_URL}/${MUT_PREFIX} -s ${GLOBAL_SEARCH_FLAG} $(BUCKET_FLAG)
 
+next-gen-stage: ## Host online for review
+	# stagel local jobs \
+	if [ -n "${PATCH_ID}" -a "${MUT_PREFIX}" = "${PROJECT}" ]; then \
+		mut-publish public ${STAGING_BUCKET} --prefix="${COMMIT_HASH}/${PATCH_ID}/${MUT_PREFIX}" --stage ${ARGS}; \
+		echo "Hosted at ${STAGING_URL}/${COMMIT_HASH}/${PATCH_ID}/${MUT_PREFIX}/${USER}/${GIT_BRANCH}/"; \
+	# reg github push and stagel commit jobs \
+	else \
+		mut-publish public ${STAGING_BUCKET} --prefix="${MUT_PREFIX}" --stage ${ARGS}; \
+		echo "Hosted at ${STAGING_URL}/${MUT_PREFIX}/${USER}/${GIT_BRANCH}/"; \
+	fi
